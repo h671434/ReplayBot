17d8cc096ac908a07c551234e54f7442b1cbd38f
ankel <spjort2003@gmail.com>
2023-09-05 15:54:41 +0200

    bigger mess

--- ReplayBot/src/test/java/replaybot/data/replay/ReadReplayStuffs.java
+++ ReplayBot/src/test/java/replaybot/data/replay/ReadReplayStuffs.java
@@ -22,37 +22,14 @@ public class ReadReplayStuffs {
 		ObjectMapper mapper = new ObjectMapper();
 		File file = new File(PATH);
 		
-		Map<String, String> spawnednames = new LinkedHashMap<>();
+		Map<String, Integer> spawnednames = null;
 		Map<String, String> updatednames = new LinkedHashMap<>();
-		int latestspawnedframe = 0;
+
 		try {
 			JsonNode root = mapper.readTree(file);
-			JsonNode frames = root.get("content").get("body").get("frames");
 			
-			frames.forEach((JsonNode f) -> {
-				f.get("replications").forEach((JsonNode r) -> {		
-					JsonNode spawned = r.get("value").get("spawned");
-					
-					if(spawned != null) {
-						String cn = spawned.get("class_name").asText();
-						String n = spawned.get("name").asText();
-						String t = spawned.get("object_name").asText();
-						
-						spawnednames.put(cn + "  " + n, t);
-					}
-					
-					JsonNode updated = r.get("value").get("updated");
-					
-					if(updated != null) {
-						updated.forEach((JsonNode u) -> {
-							String n = u.get("name").asText();
-							String t = u.get("value").fieldNames().next();
-							
-							updatednames.put(n, t);
-						});
-					}
-				});
-			});
+			spawnednames = getSpawned(root);
+			updatednames = getUpdated(root);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -61,10 +38,63 @@ public class ReadReplayStuffs {
 		String format = "%-70s | %-52s %n";
 		
 		System.out.println("Spawned : " + spawnednames.size());
-		spawnednames.forEach((String name, String type) -> System.out.format(format, name, type));
+		spawnednames.forEach((String name, Integer type) -> System.out.format(format, name, type));
 		
 		System.out.println("\nUpdated : " + updatednames.size());
 		updatednames.forEach((String name, String type) -> System.out.format(format, name, type));
+	}
+	
+	public Map<String, Integer> getSpawned(JsonNode root) {
+		Map<String, Integer> spawnednames = new LinkedHashMap<>();
+		
+		JsonNode frames = root.get("content").get("body").get("frames");
+		
+		for(int i = 0; i < frames.size(); i++) {
+			JsonNode r = frames.get(i).get("replications");
+			
+			for(int j = 0; j < r.size(); j++) {		
+				JsonNode spawned = r.get(j).get("value").get("spawned");
+				
+				if(spawned != null) {
+					String cn = spawned.get("class_name").asText();
+					String n = spawned.get("name").asText();
+					String ni = spawned.get("name_index").asText();
+					String t = spawned.get("object_name").asText();
+					String ti = spawned.get("object_id").asText();
+					
+					String k = String.format("%-39s %-30s", cn, n);
+					
+					spawnednames.put(k, spawnednames.getOrDefault(k, 0) + 1);
+				}
+			};
+		};
+		
+		return spawnednames;
+	}
+	
+	public Map<String, String> getUpdated(JsonNode root) {
+		Map<String, String> updatednames = new LinkedHashMap<>();
+		
+		JsonNode frames = root.get("content").get("body").get("frames");
+		
+		for(int i = 0; i < frames.size(); i++) {
+			JsonNode f = frames.get(i);
+			
+			f.get("replications").forEach((JsonNode r) -> {		
+				JsonNode updated = r.get("value").get("updated");
+				
+				if(updated != null) {
+					updated.forEach((JsonNode u) -> {
+						String n = u.get("name").asText();
+						String t = u.get("value").fieldNames().next();
+						
+						updatednames.put(n, t);
+					});
+				}
+			});
+		};
+		
+		return updatednames;
 	}
 
 }
