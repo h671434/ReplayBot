5bef7b47be8199949b4174fc00cf808a8aca9d27
ankel <spjort2003@gmail.com>
2023-12-12 14:04:12 +0100

    yu

--- ReplayBot/src/main/java/replaybot/replay/ballchasingapi/request/BallchasingAPIAccess.java
+++ ReplayBot/src/main/java/replaybot/replay/ballchasingapi/request/BallchasingAPIAccess.java
@@ -1,10 +1,25 @@
 package replaybot.replay.ballchasingapi.request;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 
 public class BallchasingAPIAccess {
 
-	public void requestReplayList(ReplayListRequestFilter filter) {
-		HttpRequest.
+	private static final String API_URL;
+	private static final String AUTH_TOKEN;
+	
+	HttpClient client = HttpClient.newHttpClient();
+	
+	public void requestReplayList(String params) throws URISyntaxException {
+		URI uri = new URI(API_URL + "/replays" + params);
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(uri)
+				.GET()
+				.header("Authorization", AUTH_TOKEN)
+				.build();
+		
+		
 	}
 }
